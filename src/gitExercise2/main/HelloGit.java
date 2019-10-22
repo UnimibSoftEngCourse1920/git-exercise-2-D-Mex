a2f4e71579653593b0c407cfba76aad40739de52
Davide Messina <d.messina6@campus.unimib.it>
2019-10-22 11:06:23 +0200

    Sono il primo developer e ho modificato una riga di codice

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,7 +3,7 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
-		System.out.println("QUESTA E' UNA STAMPA!");
+		System.out.println("QUESTA E' UN POLLOOOOOOOOOOOOOOO");
 		System.out.println("NON E'. UN. CANTIEEEEEERE!");
 	}
 }
